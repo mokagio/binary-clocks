c129e683e3494760003e8496cd3eef86fa4c59dd
Gio Lodi <gio@mokacoding.com>
2024-10-15 06:48:11 +0200

    Tidy: Rename for clarity and extract methods

--- ruby/binary_clock.rb
+++ ruby/binary_clock.rb
@@ -19,27 +19,31 @@ def to_4_bit_binary(number:)
     .map { |c| to_binary(number: c.to_i, bits: 4) } # convert the two digits (chars) to 4-bit binary
 end
 
-def binary_time_representation(time:, zero:, one:, spacer_string:, empty_string:)
-  # 18 will become [[0,0,0,1],[1,0,0,0]]
+def binary_clock_digits(time:)
+  # 18 will become [1, 8], then [[0,0,0,1],[1,0,0,0]]
   hours = to_4_bit_binary(number: time.hour)
   minutes = to_4_bit_binary(number: time.min)
   seconds = to_4_bit_binary(number: time.sec)
 
   # Because of the ordering of the digits array (see example above), we start from position 0 and finish at position 3
-  lines = [
+  [
     [nil, hours.last[0], nil, minutes.last[0], nil, seconds.last[0]],
     [nil, hours.last[1], minutes.first[1], minutes.last[1], seconds.first[1], seconds.last[1]],
     [hours.first[2], hours.last[2], minutes.first[2], minutes.last[2], seconds.first[2], seconds.last[2]],
     [hours.first[3], hours.last[3], minutes.first[3], minutes.last[3], seconds.first[3], seconds.last[3]]
-  ].map do |line|
-		line.map do |char|
-			if char.nil?
-				empty_string
-			else
-				char == 0 ? zero : one
-			end
-		end.join(spacer_string)
-	end.join("\n")
+  ]
+end
+
+def to_binary_clock_multiline_string(time:, zero:, one:, spacer_string:, empty_string:)
+  binary_clock_digits(time: time).map do |line|
+    line.map do |char|
+      if char.nil?
+        empty_string
+      else
+        char == 0 ? zero : one
+      end
+    end.join(spacer_string)
+  end.join("\n")
 end
 
 def print_binary_clock(zero:, one:, spacer_string:, empty_string:, show_header: true)
@@ -57,7 +61,7 @@ def print_binary_clock(zero:, one:, spacer_string:, empty_string:, show_header: 
       puts header if show_header
 
       puts <<~OUT
-        #{binary_time_representation(time: Time.now, zero:, one:, spacer_string:, empty_string:)}
+        #{to_binary_clock_multiline_string(time: Time.now, zero:, one:, spacer_string:, empty_string:)}
       OUT
 
       # Use IO.select to check if input is available without blocking
@@ -67,7 +71,7 @@ def print_binary_clock(zero:, one:, spacer_string:, empty_string:, show_header: 
         break if input == 'q'
       end
 
-      # sleep less than one second to avoid edge case when the tick is at the
+      # Sleep less than one second to avoid edge case when the tick is at the
       # second change and you go from s to s+2
       sleep 0.2
     end
@@ -77,15 +81,14 @@ def print_binary_clock(zero:, one:, spacer_string:, empty_string:, show_header: 
   end
 end
 
-mode = :emoji
+mode = :ascii
 
 case mode
 when :ascii
-	print_binary_clock(zero: 'X', one: '0', spacer_string: ' ', empty_string: ' ')
+	print_binary_clock(zero: '0', one: 'X', spacer_string: ' ', empty_string: ' ')
 when :emoji
 	# Rendering emojis results in different spaces, so we need an emoji for the empty case to keep alignment
 	print_binary_clock(zero: '⚫️', one: '🟢', spacer_string: ' ', empty_string: '⚫️', show_header: false)
 else
 	raise "Invalid mode #{mode}"
 end
-
